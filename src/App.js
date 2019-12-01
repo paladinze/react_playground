5dad324775f5ce5d49d40ca7aa73b4a44c7ee425
paladinze <paladinze@hotmail.com>
2019-12-01 19:11:58 +0800

    changed main text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ze Rocks!
+          Manny & Ze Rocks!
         </p>
       </header>
     </div>
