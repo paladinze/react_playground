5059d319008c3069633be538c4ba440d76da68d1
chengze <ze.cheng@thoughtworks.com>
2020-02-29 03:15:56 +0800

    Updates

---
+++ precache-manifest.55c2139982e600a2011cb28e4d1f166a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e8dd8d8bb759baf7018fec1e4b1cef",
+    "url": "/react_playground/index.html"
+  },
+  {
+    "revision": "04ee49d7f9dbd2a20318",
+    "url": "/react_playground/static/js/2.d7b5d449.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react_playground/static/js/2.d7b5d449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c335b58ba1f1158c0551",
+    "url": "/react_playground/static/js/main.2a2e9445.chunk.js"
+  },
+  {
+    "revision": "654ebe4b0d5af4908e26",
+    "url": "/react_playground/static/js/runtime-main.fa8b0261.js"
+  }
+]);
